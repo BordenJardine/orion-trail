0c915337d0dbcd9615db332d693631c29dda00f3
matt <matt@matt-ThinkPad.(none)>
2013-04-23 21:20:08 -0400

    Sane entity management

--- entities.js
+++ entities.js
@@ -1,4 +1,4 @@
-var shipPaths = {
+var vectorPaths = {
 	'player' : [
 		[0,0,-6],
 		[1,-6,6],
@@ -8,7 +8,7 @@ var shipPaths = {
 	]
 };
 
-var Entity = function(attr){
+var Drawable = function(attr){
 	this.pos = new Vector2(0,0);
 	this.angle = 0;
 	this.h = 8;
@@ -25,109 +25,108 @@ var Entity = function(attr){
 		ctx.fillText(this.pos.x + ', ' + this.pos.y, this.pos.x, this.pos.y);
 	};
 
-	if(attr != undefined) {
+	if(attr) {
 		for(var key in attr) {
 			if(attr.hasOwnProperty(key)){
 				this[key] = attr[key];
 			}
 		}
-	
-		if (typeof this.init == 'function') {this.init()}
 	}
+	if(typeof this.init == 'function') this.init()
 };
 
-var newPlayer = function(attr){
-	var defaults = {
-		'name' : 'player',
-		'h' : 11,
-		'w' : 11,
-		'accel' : .25,
-		'braking' : .10,
-		'handling' : .25,
-		'rotationalVel' : 0,
-		'rotationalSpeedLimit' : 5,
-		'goingForth' : 0,
-		'goingBack' : 0,
-		'rotatingLeft' : 0,
-		'rotatingRight' : 0,
-		'speedLimit' : 15,
-		'vectorPath': shipPaths.player,
-		'draw' : function(ctx){
-			ctx.save();
-			ctx.translate(this.pos.x, this.pos.y);
-			ctx.rotate(this.angle * TO_RADIANS);
-			ctx.strokeStyle = 'white';
-			ctx.beginPath();
-			for(p=0; p<this.vectorPath.length; p++){
-				if(this.vectorPath[p][0] == 0){
-					ctx.moveTo(this.vectorPath[p][1], this.vectorPath[p][2]);
-				}else{
-					ctx.lineTo(this.vectorPath[p][1], this.vectorPath[p][2]);
-				}
+var makeDrawable = function(object) {
+    object.prototype = new Drawable();
+    return new object();
+};
+
+var Player = function(attr) {
+	this.name = 'player'
+	this.h = 11
+	this.w = 11
+	this.accel = .25
+	this.braking = .10
+	this.handling = .25
+	this.rotationalVel = 0
+	this.rotationalSpeedLimit =  5
+	this.goingForth = 0
+	this.goingBack = 0
+	this.rotatingLeft = 0
+	this.rotatingRight = 0
+	this.speedLimit = 15
+	this.vectorPath = vectorPaths.player
+	this.draw = function(ctx){
+		ctx.save();
+		ctx.translate(this.pos.x, this.pos.y);
+		ctx.rotate(this.angle * TO_RADIANS);
+		ctx.strokeStyle = 'white';
+		ctx.beginPath();
+		for(p=0; p<this.vectorPath.length; p++){
+			if(this.vectorPath[p][0] == 0){
+				ctx.moveTo(this.vectorPath[p][1], this.vectorPath[p][2]);
+			}else{
+				ctx.lineTo(this.vectorPath[p][1], this.vectorPath[p][2]);
 			}
-			ctx.stroke();				
-			ctx.translate(-this.pos.x, -this.pos.y);				
-			ctx.restore();
-		},
-		'init' : function(){
-			//this.sprite.src = this.spriteSrc;
-		},
-		'update' : function() {
-			this.vel.multiplyEq(0.99);
-			this.rotationalVel *= .95;
-			var rads = this.angle * TO_RADIANS;
-			if(this.goingForth == true) {
-				if(this.vel.isMagLessThan(this.speedLimit)) { 
-					this.vel.x+= Math.sin(rads) * this.accel;
-					this.vel.y-= Math.cos(rads) * this.accel;
-				}
-			} else if(this.goingBack == true) {
-				if(this.vel.isMagLessThan(this.speedLimit)) { 
-					this.vel.x-= Math.sin(rads) * this.braking;
-					this.vel.y+= Math.cos(rads) * this.braking;
-				}
-			}
-			if(this.rotatingLeft == true) {
-				if(this.rotationalVel > -this.rotationalSpeedLimit) {
-					this.rotationalVel -= this.handling;
-				}
-			} else if(this.rotatingRight == true) {
-				if(this.rotationalVel < this.rotationalSpeedLimit) {
-					this.rotationalVel += this.handling;
-				}					
-			}
-			
-			this.angle += this.rotationalVel;
-			if(this.angle < 0) {
-				this.angle += 360;
-			} else if(this.angle >= 360) {
-				this.angle -= 360;
-			}
-			this.pos.plusEq(this.vel);
 		}
+		ctx.stroke();				
+		ctx.translate(-this.pos.x, -this.pos.y);				
+		ctx.restore();
 	};
-	
-	attr = extendObj(attr, defaults);
-	
-	return new Entity(attr);
+	this.init = function() {
+		//this.sprite.src = this.spriteSrc;
+		alert('init called');
+	};
+	this.update = function() {
+		this.vel.multiplyEq(0.99);
+		this.rotationalVel *= .95;
+		var rads = this.angle * TO_RADIANS;
+		if(this.goingForth == true) {
+			if(this.vel.isMagLessThan(this.speedLimit)) { 
+				this.vel.x+= Math.sin(rads) * this.accel;
+				this.vel.y-= Math.cos(rads) * this.accel;
+			}
+		} else if(this.goingBack == true) {
+			if(this.vel.isMagLessThan(this.speedLimit)) { 
+				this.vel.x-= Math.sin(rads) * this.braking;
+				this.vel.y+= Math.cos(rads) * this.braking;
+			}
+		}
+		if(this.rotatingLeft == true) {
+			if(this.rotationalVel > -this.rotationalSpeedLimit) {
+				this.rotationalVel -= this.handling;
+			}
+		} else if(this.rotatingRight == true) {
+			if(this.rotationalVel < this.rotationalSpeedLimit) {
+				this.rotationalVel += this.handling;
+			}					
+		}
+		
+		this.angle += this.rotationalVel;
+		if(this.angle < 0) {
+			this.angle += 360;
+		} else if(this.angle >= 360) {
+			this.angle -= 360;
+		}
+		this.pos.plusEq(this.vel);
+	};
 };
 
 
-function generateEntities(){
+function generateEntities() {
 	return [
-		newPlayer(),
-		new Entity({'name' : 'origin', 'color' : 'black', 'h' : 2, 'w': 2, 'halfH' : 1, 'halfW' : 1}),
-		new Entity({'pos' : new Vector2(50,50)}),
-		new Entity({'pos' : new Vector2(15,22)}),
-		new Entity({'pos' : new Vector2(-150,180)}),
-		new Entity({'pos' : new Vector2(620,580)}),
-		new Entity({'pos' : new Vector2(570,80)}),
-		new Entity({'pos' : new Vector2(-400,100)}),
-		new Entity({'pos' : new Vector2(170,-45)}),
-		new Entity({'pos' : new Vector2(130,370)}),
-		new Entity({'pos' : new Vector2(330,470)}),
-		new Entity({'pos' : new Vector2(230,570)}),
-		new Entity({'pos' : new Vector2(-30,-70)}),
-		new Entity({'pos' : new Vector2(650,57)})
+		makeDrawable(Player), 
+		new Drawable({'name' : 'origin', 'color' : 'black', 'h' : 2, 'w': 2, 'halfH' : 1, 'halfW' : 1}),
+		new Drawable({'pos' : new Vector2(50,50)}),
+		new Drawable({'pos' : new Vector2(15,22)}),
+		new Drawable({'pos' : new Vector2(-150,180)}),
+		new Drawable({'pos' : new Vector2(620,580)}),
+		new Drawable({'pos' : new Vector2(570,80)}),
+		new Drawable({'pos' : new Vector2(-400,100)}),
+		new Drawable({'pos' : new Vector2(170,-45)}),
+		new Drawable({'pos' : new Vector2(130,370)}),
+		new Drawable({'pos' : new Vector2(330,470)}),
+		new Drawable({'pos' : new Vector2(230,570)}),
+		new Drawable({'pos' : new Vector2(-30,-70)}),
+		new Drawable({'pos' : new Vector2(650,57)})
 	];
 }
