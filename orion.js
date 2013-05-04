39fecdb1b41ad002eac7afd0f2623b60b0664aa2
Borden Jardine <bordenjardine@gmail.com>
2013-05-03 22:40:17 -0400

    Orion Canvas added

--- orion.js
+++ orion.js
@@ -30,24 +30,14 @@ function animationLoop() {
 }
 
 
-var clearCanvas = function(alpha) {
-	alpha = alpha ? alpha : 1;
-	ctx.fillStyle='rgba(0,0,0,' + alpha + ')';
-	ctx.fillRect(0 - margin, 0 - margin, viewport.width + marginTwice, viewport.height + marginTwice);
-	return 0;
-};
-
-
 var drawDispatch = function() {
-	clearCanvas(0.9);
+	can.clearCanvas(0.9);
 	
-	ctx.fillStyle = 'white';
-	ctx.fillText('FPS: '+ fps, 5, 10);
-	ctx.save();
+	can.drawFPS(fps);
 	
-	ctx.translate(viewport.Offset.x, viewport.Offset.y);
+	can.ctx.translate(viewport.Offset.x, viewport.Offset.y);
+	can.ctx.scale(viewport.zoom, viewport.zoom);
 
-	ctx.scale(viewport.zoom, viewport.zoom);
 	var viewportDimensions = viewport.getDimensions();
 
 	for (i = entities.length - 1; i >= 0; i--) {
@@ -55,30 +45,18 @@ var drawDispatch = function() {
 			intBetween(entities[i].pos.x * viewport.zoom, viewportDimensions.l, viewportDimensions.r) &&
 			intBetween(entities[i].pos.y * viewport.zoom, viewportDimensions.t, viewportDimensions.b)
 		) {
-			entities[i].draw(ctx);
+			entities[i].draw(can.ctx);
 		}
 	}
 
-	ctx.restore();
+	can.ctx.restore();
 
 	return 0;
 };
 
 
-function resizeCanvas() {
-	if(can.width != can.clientWidth) {
-		can.width = can.clientWidth;
-		viewport.width = can.clientWidth;
-	}
-	if(can.height != can.clientHeight) {
-		can.height = can.clientHeight;
-		viewport.height = can.clientHeight;
-	}
-}
-
-
 function tickGame() {	
-	resizeCanvas();
+	can.resizeCanvas(viewport);
 	handleKeys();
 	updateEntities();
 	viewport.setZoom(player.vel.magnitude() / 25);
@@ -99,16 +77,11 @@ function updateEntities() {
 
 $(function() {
 	var $can = $('#canvas');
-	can = $can[0];
-	ctx = can.getContext('2d') 
-	ctx.font = '8px sans';
+	can = new orionCanvas($can[0]);
 	$can.attr('tabindex', 1);
-	viewport = new Viewport(can);
+	viewport = new Viewport();
 	entities = generateEntities();
 	player = entities[0];
-	
-	can.addEventListener('keydown', function(e) {e.preventDefault(); keyPresses.press(e);});
-	can.addEventListener('keyup', function(e) {e.preventDefault(); keyPresses.release(e);});
     
 	$can.focus();
 	animationLoop();
