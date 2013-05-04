dec1900f6eba6faaf7a5cb3869eaaf48285bea2a
Borden Jardine <bordenjardine@gmail.com>
2013-05-03 21:53:01 -0400

    Buoy entity

--- entities.js
+++ entities.js
@@ -8,67 +8,85 @@ var vectorPaths = {
 	]
 };
 
-var Drawable = function(attr){
-	angle = 0;
-	color = 'lightblue';
-	h = 8;
-	w = 8;
-	halfH = 4;
-	halfW = 4;
-	this.pos = new Vector2(0,0);
-	this.vel = new Vector2(0,0);
+var Drawable = function() {
+	this.angle = 0,
+		this.color = 'white',
+		this.h = 2,
+		this.w = 2,
+		this.halfH = 1,
+		this.halfW = 1,
+		this.pos = new Vector2(0,0),
+		this.vel = new Vector2(0,0);
 
 	this.draw = function(ctx){
-		ctx.fillStyle = color;
-		ctx.fillRect(this.pos.x - halfW, this.pos.y - halfH, w, h);
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.pos.x - this.halfW, this.pos.y - this.halfH, this.w, this.h);
+	};
+
+};
+
+
+var Buoy = function() {
+	this.color = 'lightblue',
+		this.h = 8,
+		this.w = 8,
+		this.halfH = 4,
+		this.halfW = 4;
+
+	this.draw = function(ctx){
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.pos.x - this.halfW, this.pos.y - this.halfH, this.w, this.h);
 
 		ctx.fillStyle = 'white';
 		ctx.fillText(this.pos.x + ', ' + this.pos.y, this.pos.x, this.pos.y);
 	};
+};
+
+
+var makeDrawable = function(object, attr) {
+	object.prototype = new Drawable();
+	var o = new object();
 
 	if(attr) {
 		for(var key in attr) {
-			if(attr.hasOwnProperty(key)){
-				this[key] = attr[key];
-			}
+			if(attr.hasOwnProperty(key)) o[key] = attr[key];
 		}
 	}
+	if(typeof o.init == 'function') o.init();
 
-	if(typeof this.init == 'function') this.init()
+	return o;
 };
 
-var makeDrawable = function(object, attr) {
-    object.prototype = new Drawable();
-    return new object();
-};
 
 var Player = function(attr) {
-	accel = .25
-	braking = .10
-	handling = .25
-	h = 11
-	w = 11
-	rotationalVel = 0
-	rotationalSpeedLimit =  5
-	speedLimit = 15
-	vectorPath = vectorPaths.player
-	this.goingForth = 0
-	this.goingBack = 0
-	this.name = 'player'
-	this.rotatingLeft = 0
-	this.rotatingRight = 0
+	this.accel = .25,
+		this.braking = .10,
+		this.color = 'white',
+		this.handling = .25,
+		this.h = 11,
+		this.w = 11,
+		this.rotationalVel = 0,
+		this.rotationalSpeedLimit = 5,
+		this.speedLimit = 15,
+		this.vectorPath = vectorPaths.player;
+		this.goingForth = 0;
+		this.goingBack = 0;
+		this.name = 'player';
+		this.rotatingLeft = 0;
+	this.rotatingRight = 0;
+
 
 	this.draw = function(ctx){
 		ctx.save();
 		ctx.translate(this.pos.x, this.pos.y);
-		ctx.rotate(angle * TO_RADIANS);
-		ctx.strokeStyle = 'white';
+		ctx.rotate(this.angle * TO_RADIANS);
+		ctx.strokeStyle = this.color;
 		ctx.beginPath();
-		for(p=0; p<vectorPath.length; p++){
-			if(vectorPath[p][0] == 0){
-				ctx.moveTo(vectorPath[p][1], vectorPath[p][2]);
+		for(var p=0; p<this.vectorPath.length; p++){
+			if(this.vectorPath[p][0] == 0){
+				ctx.moveTo(this.vectorPath[p][1], this.vectorPath[p][2]);
 			}else{
-				ctx.lineTo(vectorPath[p][1], vectorPath[p][2]);
+				ctx.lineTo(this.vectorPath[p][1], this.vectorPath[p][2]);
 			}
 		}
 		ctx.stroke();				
@@ -79,34 +97,34 @@ var Player = function(attr) {
 
 	this.update = function() {
 		this.vel.multiplyEq(0.99);
-		rotationalVel *= .95;
-		var rads = angle * TO_RADIANS;
+		this.rotationalVel *= .95;
+		var rads = this.angle * TO_RADIANS;
 		if(this.goingForth == true) {
-			if(this.vel.isMagLessThan(speedLimit)) { 
-				this.vel.x+= Math.sin(rads) * accel;
-				this.vel.y-= Math.cos(rads) * accel;
+			if(this.vel.isMagLessThan(this.speedLimit)) { 
+				this.vel.x+= Math.sin(rads) * this.accel;
+				this.vel.y-= Math.cos(rads) * this.accel;
 			}
 		} else if(this.goingBack == true) {
 			if(this.vel.isMagLessThan(speedLimit)) { 
-				this.vel.x-= Math.sin(rads) * braking;
-				this.vel.y+= Math.cos(rads) * braking;
+				this.vel.x-= Math.sin(rads) * this.braking;
+				this.vel.y+= Math.cos(rads) * this.braking;
 			}
 		}
 		if(this.rotatingLeft == true) {
-			if(rotationalVel > -rotationalSpeedLimit) {
-				rotationalVel -= handling;
+			if(this.rotationalVel > -this.rotationalSpeedLimit) {
+				this.rotationalVel -= this.handling;
 			}
 		} else if(this.rotatingRight == true) {
-			if(rotationalVel < rotationalSpeedLimit) {
-				rotationalVel += handling;
+			if(this.rotationalVel < this.rotationalSpeedLimit) {
+				this.rotationalVel += this.handling;
 			}					
 		}
 		
-		angle += rotationalVel;
-		if(angle < 0) {
-			angle += 360;
-		} else if(angle >= 360) {
-			angle -= 360;
+		this.angle += this.rotationalVel;
+		if(this.angle < 0) {
+			this.angle += 360;
+		} else if(this.angle >= 360) {
+			this.angle -= 360;
 		}
 		this.pos.plusEq(this.vel);
 	};
@@ -116,18 +134,18 @@ var Player = function(attr) {
 function generateEntities() {
 	return [
 		makeDrawable(Player), 
-		new Drawable({'name' : 'origin', 'color' : 'black', 'h' : 2, 'w': 2, 'halfH' : 1, 'halfW' : 1}),
-		new Drawable({'pos' : new Vector2(50,50)}),
-		new Drawable({'pos' : new Vector2(15,22)}),
-		new Drawable({'pos' : new Vector2(-150,180)}),
-		new Drawable({'pos' : new Vector2(620,580)}),
-		new Drawable({'pos' : new Vector2(570,80)}),
-		new Drawable({'pos' : new Vector2(-400,100)}),
-		new Drawable({'pos' : new Vector2(170,-45)}),
-		new Drawable({'pos' : new Vector2(130,370)}),
-		new Drawable({'pos' : new Vector2(330,470)}),
-		new Drawable({'pos' : new Vector2(230,570)}),
-		new Drawable({'pos' : new Vector2(-30,-70)}),
-		new Drawable({'pos' : new Vector2(650,57)})
+		makeDrawable(Buoy, {'name' : 'origin', 'color' : 'black', 'h' : 2, 'w': 2, 'halfH' : 1, 'halfW' : 1}),
+		makeDrawable(Buoy, {'pos' : new Vector2(50,50)}),
+		makeDrawable(Buoy, {'pos' : new Vector2(15,22)}),
+		makeDrawable(Buoy, {'pos' : new Vector2(-150,180)}),
+		makeDrawable(Buoy, {'pos' : new Vector2(620,580)}),
+		makeDrawable(Buoy, {'pos' : new Vector2(570,80)}),
+		makeDrawable(Buoy, {'pos' : new Vector2(-400,100)}),
+		makeDrawable(Buoy, {'pos' : new Vector2(170,-45)}),
+		makeDrawable(Buoy, {'pos' : new Vector2(130,370)}),
+		makeDrawable(Buoy, {'pos' : new Vector2(330,470)}),
+		makeDrawable(Buoy, {'pos' : new Vector2(230,570)}),
+		makeDrawable(Buoy, {'pos' : new Vector2(-30,-70)}),
+		makeDrawable(Buoy, {'pos' : new Vector2(650,57)})
 	];
 }
